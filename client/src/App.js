d9a28cbc9f325b6307631ffad7f2f04c0c6a0ac0
S. M. Ahad Ali Chowdhury <77457836+ahadalichowdhury@users.noreply.github.com>
2023-10-30 21:19:03 +0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,12 @@ import { useState } from 'react';
 function App() {
 
   const [user,setUser]= useState([])
-  axios.get(`http://localhost:5000/api/v1/user`).then((res)=>{
+  axios.get(`http://54.172.237.132:5000/api/v1/user`).then((res)=>{
     setUser(res.data.data)
   })
+
+  //http://54.172.237.132:5000/api/v1/user  this is for testing live backend url
+  //http://localhost:5000/api/v1/user
   return (
     <div>
       hello world
